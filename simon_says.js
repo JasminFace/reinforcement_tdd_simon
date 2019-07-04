524457626d150ba39d9fcba0989015d2c532fea5
Jasmin Saromo <jasminsaromo@gmail.com>
2019-07-04 15:36:08 -0400

    repeat test passed

--- simon_says.js
+++ simon_says.js
@@ -6,4 +6,8 @@ const shout = (string) => {
   return string.toUpperCase();
 };
 
+const repeat = (word, num) => {
+  return (word + ' ').repeat(num).trim();
+};
+
 module.exports = {echo: echo, shout: shout, repeat: repeat, startOfWord: startOfWord, firstWord: firstWord};
