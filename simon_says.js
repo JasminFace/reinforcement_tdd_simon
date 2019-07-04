405f662282e2743f71b0ec638dcf2b57b11e5775
Jasmin Saromo <jasminsaromo@gmail.com>
2019-07-04 15:35:36 -0400

    shout test passed

--- simon_says.js
+++ simon_says.js
@@ -2,4 +2,8 @@ const echo = (word) => {
   return word;
 };
 
+const shout = (string) => {
+  return string.toUpperCase();
+};
+
 module.exports = {echo: echo, shout: shout, repeat: repeat, startOfWord: startOfWord, firstWord: firstWord};
